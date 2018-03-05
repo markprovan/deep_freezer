442d00b6e020b0610a14b8549a4b1c664ffad396
Mark Provan <mark.provan@itison.com>
2018-03-05 15:04:44 +0000

    Reintroduce Defrost and bump version

---
+++ lib/deep_freezer/defrost.rb
@@ -0,0 +1,40 @@
+class DeepFreezer::Defrost
+
+  def self.load!
+
+    path = DeepFreezer::Base.fixture_path.join("**/*.yml")
+    Dir.glob(path).each do |file|
+      objects = YAML.load(File.read(file))
+      puts "Loading #{objects.first.keys.first}"
+      objects.each do |o|
+
+        klass = o.keys.first
+        attrs = o[klass]
+        obj = klass.constantize.new
+
+        attrs.keys.each do |a|
+          obj.send("#{a}=", attrs[a])
+        end
+
+        sql = self.sql_for(obj)
+        ActiveRecord::Base.connection.execute sql
+      end
+    end
+
+  end
+
+  def self.sql_for(record)
+    values = record.send(:arel_attributes_with_values_for_create, record.attribute_names)
+
+    model = record.class
+    scope = model.unscoped
+
+    im = scope.arel.create_insert
+    im.into model.arel_table
+
+    substitutes, binds = scope.substitute_values(values)
+    im.insert substitutes
+
+    record.class.connection.to_sql(im, binds)
+  end
+end
