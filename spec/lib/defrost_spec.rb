267fb73c95a556d5385113d138af4ff72f145318
Mark Provan <mark.provan@itison.com>
2018-03-06 14:49:09 +0000

    Refactor loading YAML hash to object. Add specs

--- spec/lib/defrost_spec.rb
+++ spec/lib/defrost_spec.rb
@@ -8,6 +8,21 @@ RSpec.describe DeepFreezer::Defrost do
     end
   end
 
+  describe "Loading YAML" do
+    
+    let(:parsed_yaml) { YAML.load("\n- Test:\n    id: 1\n    name: Mark\n").first }
+    let(:instance) { described_class.hash_to_instance(parsed_yaml) }
+
+    it "creates the correct type of model instance" do
+      expect(instance).to be_a(Test)
+    end
+
+    it "correctly sets each attribute" do
+      expect(instance.id).to eql 1
+      expect(instance.name).to eql "Mark"
+    end
+  end
+
   describe 'SQL Conversion' do
     let(:test_model) do
       class Test < ActiveRecord::Base;end
